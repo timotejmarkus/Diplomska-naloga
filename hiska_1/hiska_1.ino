1d9b5fef88e00d656f2f9be7241e4d4cfff52a74
timotejmarkus <janez1234.markus@gmail.com>
2021-11-24 16:56:26 +0100

    Če je svetlo prižgi ventilator

--- hiska_1/hiska_1.ino
+++ hiska_1/hiska_1.ino
@@ -246,7 +246,9 @@ void auto_sensor() {
   }
   light = analogRead(A1);////Assign the analog value of A1 to light
   if (light < 300)//if variable light is less than 300
+
   {
+    fan_stop ();
     infrar = digitalRead(2);//assign the value of digital 2 to infrar 
     Serial.println(infrar);//output the value of variable infrar in new lines
     if (infrar == 1)
@@ -258,12 +260,9 @@ void auto_sensor() {
       digitalWrite(13, LOW); //set digital 13 to low level, LED is off 
     }
 
-  }  else{
-    digitalWrite(13, LOW);
-    digitalWrite(7, LOW);
-      digitalWrite(6, HIGH);
-    }
+  }  else{fan_start();
     
+  }
   water = analogRead(A3);//assign the analog value of A3 to variable water
   if (water > 800)
     // if variable water is larger than 800
@@ -455,7 +454,16 @@ void Ode_to_Joy()//play Ode to joy song
     delay(300 * durt[x]);
   }
 }
-
+void fan_start(){
+  digitalWrite (7, HIGH);
+   digitalWrite (6, LOW);
+   
+   }
+   void fan_stop(){
+  digitalWrite (7, LOW);
+   digitalWrite (6, LOW); 
+   
+   }
 //PWM control
 void pwm_control() {
   switch (val)

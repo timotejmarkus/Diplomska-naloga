1a725feac89f3561b88504f03b1b5fa28691e39c
timotejmarkus <janez1234.markus@gmail.com>
2021-11-24 15:59:12 +0100

    Sprememba alarma

--- hiska_1/hiska_1.ino
+++ hiska_1/hiska_1.ino
@@ -203,48 +203,12 @@ void loop() {
 ////////////////////////set birthday song//////////////////////////////////
 void birthday()
 {
-  tone(3, 294); //digital 3 outputs 294HZ sound 
-  delay(250);//delay in 250ms
-  tone(3, 440);
-  delay(250);
-  tone(3, 392);
-  delay(250);
-  tone(3, 532);
-  delay(250);
-  tone(3, 494);
-  delay(500);
-  tone(3, 392);
-  delay(250);
-  tone(3, 440);
-  delay(250);
-  tone(3, 392);
-  delay(250);
-  tone(3, 587);
-  delay(250);
-  tone(3, 532);
-  delay(500);
-  tone(3, 392);
-  delay(250);
-  tone(3, 784);
-  delay(250);
-  tone(3, 659);
-  delay(250);
-  tone(3, 532);
-  delay(250);
-  tone(3, 494);
-  delay(250);
-  tone(3, 440);
-  delay(250);
-  tone(3, 698);
-  delay(375);
-  tone(3, 659);
-  delay(250);
-  tone(3, 532);
-  delay(250);
-  tone(3, 587);
-  delay(250);
-  tone(3, 532);
-  delay(500);
+  for(int i=0;i<10;i++){
+    tone(3,300+i*50);
+    delay(100);
+  }
+
+  
 }
 
 
